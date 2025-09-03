1ec6bca5ea1d0439aae0aa5c4477ee0173d25c4d
Ing.Victor Eduardo Resendiz Villegas <victoreduardoresendizvillegas@gmail.com>
2025-09-03 07:13:19 -0600

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Import utilities from `astro:content`
-import {defineCollection, reference, z} from 'astro:content';
+import { defineCollection, reference, z } from 'astro:content';
 
 // 2. Import loader(s)
 import { glob } from 'astro/loaders';
@@ -27,14 +27,14 @@ const projects = defineCollection({
     schema: ({ image }) => z.object({
         title: z.string(),
         startDate: z.date(),
-        endDate: z.date(),
+        endDate: z.date().optional(),
         summary: z.string(),
-        url: z.string(),
-        cover: image(),
+        url: z.string().optional(),
+        cover: image().optional(),
         tags: z.array(z.string()),
-        ogImage: z.string()
+        ogImage: z.string().optional()
     }),
 });
 
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = {  projects, posts };
+export const collections = { posts, projects };
