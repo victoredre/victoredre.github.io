a1af9271a1e56f05517733f7932afe0d5a9e8a2e
Ing.Victor Eduardo Resendiz Villegas <victoreduardoresendizvillegas@gmail.com>
2025-09-03 07:37:08 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,33 @@
 // @ts-check
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://victoredre.github.io/',
-    integrations: [mdx(), react(), tailwind({
-        applyBaseStyles: false,
-    }), icon(), sitemap()],
+    site: 'https://victoredre.github.io',
+    base: '/',
+    output: 'static',
+    integrations: [
+        mdx(), 
+        react(), 
+        tailwind({
+            applyBaseStyles: false,
+        }), 
+        icon(), 
+        sitemap()
+    ],
     markdown: {
         shikiConfig: {
             theme: 'plastic',
             wrap: true,
         },
+    },
+    build: {
+        assets: '_astro'
     },
     experimental: {
         svg: true,
