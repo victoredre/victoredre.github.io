95a43dcf801f17a6c23f1d5a32cc3d9a5f0543c3
Ing.Victor Eduardo Resendiz Villegas <victoreduardoresendizvillegas@gmail.com>
2026-06-11 00:29:44 -0600

    Update education.ts

--- src/data/education.ts
+++ src/data/education.ts
@@ -13,7 +13,7 @@ const  education: Education[] = [
         title: "Universidad Tecnológica de Nezahualcoyotl",
         startDate: "2023-09-01",
         endDate: "2028-11-01",
-        school: "Ingeniero en Tecnologías de la Información y Comunicaciónes",
+        school: "Ing. Tecnologías de la Información y Comunicaciónes",
         location: "Estado de México",
         description: "Espealidad en diseño, desarrollo, implementación e integración de servicios ciberseguridad",
         currentUni: true,
