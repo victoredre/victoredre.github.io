65757d207a112867634246eab8b4590836b90236
Ing.Victor Eduardo Resendiz Villegas <victoreduardoresendizvillegas@gmail.com>
2026-07-21 04:49:35 -0600

    Update icons for hard skills in hardSkills.ts

--- src/data/hardSkills.ts
+++ src/data/hardSkills.ts
@@ -8,37 +8,37 @@ const hardSkills: HardSkill[] = [
   {
     name: "Fortinet",
     description: "FortiAnalyzer, FortiExtender, FortiManager, FortiSwitch,  FortiAP,  FortiWeb,  FortiGate,  FortiWifi",
-    icon: "astro_fortine"
+    icon: "astro_dark"
   },
   {
     name: "WatchGuard",
     description: "My favorite React framework for building modern web apps",
-    icon: "astro_watch"
+    icon: "twitter-x-fill"
   },
   {
     name: "Palo Alto Network",
     description: "I've been using NodeJs for backend development since 2018",
-    icon: "astro_paloalto"
+    icon: "instagram-fill"
   },
   {
     name: "Juniper",
     description: "I've been using NodeJs for backend development since 2018",
-    icon: "astro_juniper"
+    icon: "facebook-fill"
   },
     {
     name: "Trend Micro",
     description: "I've been using NodeJs for backend development since 2018",
-    icon: "astro_trendmicro"
+    icon: "youtube-fill"
   },
     {
     name: "Kaspersky",
     description: "I've been using NodeJs for backend development since 2018",
-    icon: "astro_trendmicro"
+    icon: "nodejs"
   },
   {
     name: "Check Point",
     description: "My favorite database for modern apps",
-    icon: "astro_checkpoint"
+    icon: "mongodb"
   }
 ];
 
