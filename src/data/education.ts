b800e3ff0e8805f6392bbdf68012b218748a3f96
Ing.Victor Eduardo Resendiz Villegas <victoreduardoresendizvillegas@gmail.com>
2026-07-31 00:07:02 -0600

    Update education.ts

--- src/data/education.ts
+++ src/data/education.ts
@@ -11,11 +11,11 @@ interface Education {
 const  education: Education[] = [
     {
         title: "Universidad Tecnológica de Nezahualcoyotl",
-        startDate: "2013-09-01",
-        endDate: "2017-08-01",
-        school: "Carrera: Tecnologías de la Información y Comunicaciónes",
-        location: "Direccion: Colonia Benito Juárez, C.P. 57000, Ciudad Nezahualcóyotl, Estado de México.",
-        description: "Espealidad: Desarrollo, Implementación e Integración de Servicios Ciberseguridad.",
+        startDate: "2013-01-10",
+        endDate: "2017-12-01",
+        school: "Tecnologías de la Información y Comunicación",
+        location: "Ciudad Nezahualcóyotl, Estado de Méx.",
+        description: "Desarrollo, Implementación e Integración de Servicios Ciberseguridad.",
         currentUni: false,
     },
 ];
