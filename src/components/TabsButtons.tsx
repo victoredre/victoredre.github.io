78f65f1285d649146734d916ce3ec4f7bc20e560
Ing.Victor Eduardo Resendiz Villegas <victoreduardoresendizvillegas@gmail.com>
2026-07-30 23:20:47 -0600

    Update TabsButtons.tsx

--- src/components/TabsButtons.tsx
+++ src/components/TabsButtons.tsx
@@ -2,13 +2,13 @@ import {Tabs, TabsContent, TabsList, TabsTrigger} from "./ui/tabs.tsx";
 
 const TabsButtons = (props:any) => {
     return (
-        <Tabs defaultValue="Certificaciones" className="w-full ">
+        <Tabs defaultValue="portfolio" className="w-full ">
             <TabsList className={"bg-white dark:bg-n700  w-full px-2 py-8 rounded-xl"}>
-                <TabsTrigger className={"w-full py-4 rounded-lg dark:data-[state=active]:text-primary-dark data-[state=active]:bg-light-theme dark:data-[state=active]:bg-dark-theme"} value="Certificaciones">Certificaciones</TabsTrigger>
+                <TabsTrigger className={"w-full py-4 rounded-lg dark:data-[state=active]:text-primary-dark data-[state=active]:bg-light-theme dark:data-[state=active]:bg-dark-theme"} value="portfolio">portfolio</TabsTrigger>
                 <TabsTrigger className={"w-full py-4 rounded-lg dark:data-[state=active]:text-primary-dark data-[state=active]:bg-light-theme dark:data-[state=active]:bg-dark-theme"} value="about">About</TabsTrigger>
             </TabsList>
-            <TabsContent value="Certificaciones">
-                {props.Certificaciones}
+            <TabsContent value="portfolio">
+                {props.portfolio}
             </TabsContent>
             <TabsContent value="about">
                 {props.about}
