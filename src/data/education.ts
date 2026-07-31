c4cbceee548aa4c0027a671ee21f800b8a56a2bb
Ing.Victor Eduardo Resendiz Villegas <victoreduardoresendizvillegas@gmail.com>
2026-07-31 01:47:32 -0600

    Update education.ts

--- src/data/education.ts
+++ src/data/education.ts
@@ -13,9 +13,9 @@ const  education: Education[] = [
         title: "Universidad Tecnológica de Nezahualcoyotl",
         startDate: "2013-01-10",
         endDate: "2017-12-01",
-        school: "Ingenieria en Tecnologías de la Información y Comunicación. |",
-        location: "Divición Telematica.",
-        description: " Dirección: Circuito Universidad Tecnológica s/n, Colonia Benito Juárez, C.P. 57000, Ciudad Nezahualcóyotl, Estado de México | Espealidad: Desarrollo, Implementación e Integración de Servicios Ciberseguridad.",
+        school: "Ingenieria en Tecnologías de la Información y Comunicación.",
+        location: "Telematica.",
+        description: " Dirección: Circuito Universidad Tecnológica s/n, Colonia Benito Juárez, C.P. 57000, Ciudad Nezahualcóyotl, Estado de México | Especialidad: Desarrollo, Implementación e Integración de Servicios Ciberseguridad.",
         currentUni: false,
     },
 ];
